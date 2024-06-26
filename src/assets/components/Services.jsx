623f724b0d2f39e800f73712f382ccd83abf3cab
Filippoalotta <alottafilippo@gmail.com>
2024-06-26 17:08:27 +0200

    Modificata sezione Services, rimpiazzate immagini, paragrafi e titoli

--- src/assets/components/Services.jsx
+++ src/assets/components/Services.jsx
@@ -1,28 +1,34 @@
 import '../scss/Services.scss'
-import devIcon from '../images/dev-icon.png'
-import seoIcon from '../images/seo-icon.png'
-import supIcon from '../images/sup-icon.png'
+import reactIcon from '../images/react.svg'
+import apiIcon from '../images/api-img.png'
+import gitHubIcon from '../images/github-mark.svg'
 
 function Services(){
 
     return(
         <>
             <section id='services' className="services-container">
-                <h3>Web Developer</h3>
-                <h2>Filippo Alotta Web Developer</h2>
-                <p>Sono Filippo Alotta, esperto nello sviluppo di siti web che valorizzano l&apos;identità online di aziende, imprese e professionisti.</p>
+                <h3>Filippo Alotta</h3>
+                <h2>Front End React Developer</h2>
+                <p>Sono un front-end developer specializzato nello sviluppo di esperienze utente intuitive e responsive.</p>
                 <div className="services-content-container">
                     <div className='services-content'>
-                        <img src={devIcon} alt="web development and design icon" />
-                        <p>Sviluppo siti web che si distinguono, con un design accattivante e funzionalità intuitive.</p>
+                        <a href="https://it.legacy.reactjs.org/" rel='noopener' target='_blank'>
+                            <img src={reactIcon} alt="React.js icon" />
+                        </a>
+                        <p>Creo interfacce utente interattive utilizzando React, sfruttando componenti riutilizzabili.</p>
                     </div>
                     <div className='services-content'>
-                        <img src={seoIcon} alt="web development and design icon" />
-                        <p>Aumenta la tua visibilità con strategie SEO personalizzate per posizionarti al top sui motori di ricerca.</p>
+                        <a href="#">
+                            <img src={apiIcon} alt="API integration icon" />
+                        </a>
+                        <p>Esperto nell&apos;integrazione di API e nella gestione dello stato delle applicazioni con React.</p>
                     </div>
                     <div className='services-content'>
-                        <img src={supIcon} alt="web development and design icon" />
-                        <p>Sicurezza e tranquillità garantite. Un supporto tecnico per ogni esigenza del tuo sito.</p>
+                        <a href="https://github.com/Filippoalotta" rel='noopener' target='_blank'>
+                            <img src={gitHubIcon} alt="GitHub icon" />
+                        </a>
+                        <p>Utilizzo Git e piattaforme come GitHub per il versionamento del codice e il controllo delle versioni.</p>
                     </div>
                 </div>
             </section>
