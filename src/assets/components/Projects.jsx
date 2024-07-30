4ec496be55237715905d059200ab69f8262856e3
Filippoalotta <alottafilippo@gmail.com>
2024-07-30 14:41:50 +0200

    added netflix project

--- src/assets/components/Projects.jsx
+++ src/assets/components/Projects.jsx
@@ -1,6 +1,7 @@
 import '../scss/Projects.scss'
 import portfolio from '../images/portfolio-screen.png'
 import carRental from '../images/carrental-screen.png'
+import netflix from '../images/netflix-screen.png'
 import agEvent from '../images/agevent-screen.png'
 
 function Projects() {
@@ -9,7 +10,7 @@ function Projects() {
         <>
             <section id='projects' className="projects-page">
                 <div className="projects-container">
-                   <a href="https://filippoalotta.netlify.app/" target='_blank' rel='noopener'>
+                   <a href="https://github.com/Filippoalotta/Portfolio" target='_blank' rel='noopener'>
                         <div className="project">
                             <img src={portfolio} alt="portfolio project" />
                             <h3>Portfolio</h3>
@@ -20,7 +21,18 @@ function Projects() {
                             </div>
                         </div>
                    </a>
-                   <a href="https://carrentalau.netlify.app/" target='_blank' rel='noopener'>
+                   <a href="https://github.com/Filippoalotta/Netflix-clone" target='_blank' rel='noopener'>
+                        <div className="project">
+                            <img src={netflix} alt="Netflix clone" />
+                            <h3>Netflix clone</h3>
+                            <div className="skills-container">
+                                <span>React</span>
+                                <span>Sass</span>
+                                <span>Firebase</span>
+                            </div>
+                        </div>
+                   </a>
+                   <a href="https://github.com/Filippoalotta/Car-rental" target='_blank' rel='noopener'>
                         <div className="project">
                             <img src={carRental} alt="car rental project" />
                             <h3>Car Rental</h3>
@@ -31,7 +43,7 @@ function Projects() {
                             </div>
                         </div>
                    </a>
-                   <a href="https://agevent.netlify.app/" target='_blank' rel='noopener'>
+                   <a href="https://github.com/Filippoalotta/agevents" target='_blank' rel='noopener'>
                         <div className="project">
                             <img src={agEvent} alt="event agency project" />
                             <h3>AGevent</h3>
