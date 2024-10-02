c5713a5c68706e0359216ebb8eadae333c64e053
Filippoalotta <alottafilippo@gmail.com>
2024-10-02 18:01:59 +0200

    cambiata città

--- src/assets/components/Banner.jsx
+++ src/assets/components/Banner.jsx
@@ -8,7 +8,7 @@ function Banner(){
             <section id='home' className="banner-container">
                 <div className="banner-content">
                     <h1>Front-End React Developer 💻</h1>
-                    <p>Ciao, sono Filippo Alotta, un Front-end React Developer di Palermo.</p>
+                    <p>Ciao, sono Filippo Alotta, un Front-end React Developer di Roma.</p>
                     <div className="skills-container">
                         <span>HTML</span>
                         <span>CSS</span>
